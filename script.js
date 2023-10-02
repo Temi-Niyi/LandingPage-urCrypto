4e9446ce00a42b77ab6d8e02a15fa3dc84ebfb9b
Temi-Niyi <104735287+Temi-Niyi@users.noreply.github.com>
2023-10-02 09:14:21 +0100

    first commit

--- script.js
+++ script.js
@@ -6,43 +6,31 @@ const buttonsContainer = document.getElementById("buttonsContainer");
 
 let menuVisible = false;
 
+function adjustDisplay() {
+  if (window.innerWidth >= 768) {
+    menuBtn.style.display = "none";
+    loginButton.style.display = "inline-block";
+    registerButton.style.display = "inline-block";
+  } else {
+    menuBtn.style.display = "block";
+    loginButton.style.display = "none";
+    registerButton.style.display = "none";
+  }
+}
+
 menuBtn.addEventListener("click", function () {
   if (menuVisible) {
     menu.style.display = "none";
-    menuBtn.textContent = "☰"; // Change text to "☰"
+    menuBtn.textContent = "☰";
   } else {
     menu.style.display = "block";
-    menuBtn.textContent = "x"; // Change text to "x"
+    menuBtn.textContent = "x";
   }
   menuVisible = !menuVisible;
 });
 
+// Call the adjustDisplay function on page load
+adjustDisplay();
+
 // Add event listener to window resize to handle changes in screen width
-window.addEventListener("resize", function () {
-  if (window.innerWidth <= 781) {
-    // Hide login and register buttons
-    loginButton.style.display = "none";
-    registerButton.style.display = "none";
-    
-    // Show the menuBtn
-    menuBtn.style.display = "block";
-  } else {
-    // Restore initial state when screen width is larger than 600px
-    menuBtn.style.display = "none";
-    menu.style.display = "block";
-    menuVisible = false;
-    loginButton.style.display = "inline-block";
-    registerButton.style.display = "inline-block";
-  }
-});
-
-
-
-// Initial check on page load
-if (window.innerWidth <= 781) {
-  menuBtn.style.display = "block";
-}else{
-  menuBtn.style.display = "none";
-
-}
-
+window.addEventListener("resize", adjustDisplay);
